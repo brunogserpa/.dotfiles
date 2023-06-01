6b7cde7943eab208c9af2748efb94327b58c5fe2
Bruno Serpa <brunogserpa@gmail.com>
2023-06-01 14:03:00 -0300

    REALLY added eww settings and started script for installing manjaro

---
+++ init.sh
@@ -0,0 +1,24 @@
+sudo pacman -S hyprland
+    cargo
+    sddm
+    xdg-desktop-portal-hyprland
+    gtk3 
+    gtk-layer-shell
+    pango 
+    cairo 
+    glib2 
+    gcc-libs 
+    glibc
+    pkg-config
+
+yay -S eww-wayland ttf-ubuntu-nerd socat jq acpi inotify-tools\
+  bluez pavucontrol brightnessctl playerctl nm-connection-editor imagemagick \
+  gjs gnome-bluetooth-3.0 upower networkmanager gtk3 \
+  wl-gammactl wlsunset wl-clipboard hyprpicker hyprshot blueberry \
+  polkit-gnome
+
+systemctl enable sddm
+
+git clone https://github.com/elkowar/eww
+cd eww
+cargo build --release --no-default-features --features=wayland
