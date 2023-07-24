146466b1530afbe29ca7a6ef6e83c2ebd11a23ce
Bruno Serpa <brunogserpa@gmail.com>
2023-07-24 12:49:21 -0300

    [Script] Modified manjaro install script | Removed unused files

--- configs/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-THEME="minimal"
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=~/.config/polybar/themes/$THEME/config.ini
-polybar main -c $CONFIG_DIR &
