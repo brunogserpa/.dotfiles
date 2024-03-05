6f93138aeb1caa88b2b42816b7736934201c39ba
Bruno Serpa <brunogserpa@gmail.com>
2024-03-05 18:42:28 -0300

    [Waybar] Removed borders and decreased height

--- configs/waybar/modules/spotify.sh
+++ configs/waybar/modules/spotify.sh
@@ -3,10 +3,7 @@
 class=$(playerctl metadata --player=spotify --format '{{lc(status)}}')
 
 if [[ $class == "playing" ]]; then
-  info=$(playerctl metadata --player=spotify --format ' {{artist}} - {{title}}')
-  if [[ ${#info} > 40 ]]; then
-    info=$(echo $info | cut -c1-40)"..."
-  fi
+  info=$(playerctl metadata --player=spotify --format '♪ {{artist}} ⁝ {{title}}')
   text=$info
 elif [[ $class == "paused" ]]; then
   text=$icon
